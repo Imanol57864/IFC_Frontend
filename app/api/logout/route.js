f3cd42a7bca4a25d1f3efe503736e33e24876a01
Imanol57864 <alfonsochoxd@gmail.com>
2026-06-21 21:14:38 -0600

    Triying to fix request headers issue

--- app/api/logout/route.js
+++ app/api/logout/route.js
@@ -1,6 +1,17 @@
 import { NextResponse } from "next/server";
 
 export async function GET(request) {
+  console.log({
+    url: request.url,
+    host: request.headers.get("host"),
+    xfh: request.headers.get("x-forwarded-host"),
+    xfp: request.headers.get("x-forwarded-proto"),
+  });
+  const host = request.headers.get("host");
+  const proto = request.headers.get("x-forwarded-proto") ?? "http";
+  const baseUrl = `${proto}://${host}`;
+  console.log(baseUrl);
+  ///
   const response = NextResponse.redirect(new URL("/login", request.url), { status: 303 });
   response.cookies.delete("access_token");
   response.cookies.delete("refresh_token");
